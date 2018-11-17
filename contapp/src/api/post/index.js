a6aa428eb2d28bcc7ebb66a6090333e090afde02
Erick Muñoz <ermuoz1014@gmail.com>
2018-11-16 21:33:41 -0600

    add

--- contapp/src/api/post/index.js
+++ contapp/src/api/post/index.js
@@ -1,18 +1,38 @@
 export function post(type,data){
     let url ='http://localhost:10010/';
+    const API_HOST = process.env.NODE_ENV === 'production' ? 'www.algo.com' : 'localhost'
+    const API_PORT = process.env.API_PORT || '10010'
+    const API_URL = `http://${API_HOST}:${API_PORT}/`
     
 return new Promise((resolve,reject)=>{
-        fetch(url+type,{
+        fetch(API_URL+type,{
             method:'POST',
             headers:{"Content-Type": "application/json; charset=utf-8"},
             body:JSON.stringify(data)
         })
-        .then((response) => response.json())
-        .then((responseJSON)=>{
-            resolve(responseJSON);
+        .then(res => {
+            if (res.status!==200){
+                reject(res.status);
+            }else{
+                return res.text();
+            }
+        })
+        .then(body =>{
+            let res =JSON.parse(body);
+            if(!res.success){
+                reject(res.error);
+            }
+            
         }).catch((error)=>{
             reject(error);
         })
     })
+
+}
+
+export function getToken () {
+    return {
+        'X-API-KEY': window.localStorage('API_TOKEN')
+    }
 
 }
